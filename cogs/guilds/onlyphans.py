2f4f22c6199735fa85c61108261e7cdcf4852f13
Cesar Reynosa <creynosa@protonmail.com>
2021-11-23 17:11:57 -0600

    Add updated logging methods for commands in wigglyfriends

--- cogs/guilds/onlyphans.py
+++ cogs/guilds/onlyphans.py
@@ -17,65 +17,62 @@ class OnlyPhans(commands.Cog):
         return ctx.guild.id == 733944519640350771
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def eversong(self, ctx):
-        logger.info(f"{ctx.author.name} used the eversong command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/HijmLV8.jpg")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def bagu4k(self, ctx):
-        logger.info(f"{ctx.author.name} used the bagu4k command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/Wp9aJ4q.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def enter(self, ctx):
-        logger.info(f"{ctx.author.name} used the enter command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/tNSoLk5.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def marshiie(self, ctx):
-        logger.info(f"{ctx.author.name} used the marshiie command.")
-
         marshiieID = 142809966749679616
-        marshiieUser = await self.bot.fetch_user(marshiieID)
+        marshiie = await self.bot.fetch_user(marshiieID)
 
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/UgiwdJV.png")
         await ctx.send(
             f"Introducing: the <:ayaya:745792801215610930> Ayaya"
-            f"{marshiieUser.mention} Collection™ <:ayaya:745792801215610930>",
+            f"{marshiie.mention} Collection™ <:ayaya:745792801215610930>",
             embed=embed,
         )
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def maniicc(self, ctx):
-        logger.info(f"{ctx.author.name} used the maniic command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/k94JJVU.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def kat(self, ctx):
-        logger.info(f"{ctx.author.name} used the kat command.")
-        embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/FJgfFTi.png")
-        await ctx.send(embed=embed)
+        await ctx.send(content='<:why:745691113939009638>')
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def pusher(self, ctx):
-        logger.info(f"{ctx.author.name} used the pusher command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/0mHp2wx.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def neph(self, ctx):
-        logger.info(f"{ctx.author.name} used the neph command.")
         embed = helpers.makeSimpleTextEmbed("<a:loading:868329043081183272> Loading...")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def crab(self, ctx):
-        logger.info(f"{ctx.author.name} used the crab command.")
-
         oldNick = ctx.author.nick
         if oldNick is None:
             oldNick = str(ctx.author.name)
@@ -91,9 +88,8 @@ class OnlyPhans(commands.Cog):
         await ctx.send("https://media.giphy.com/media/2dK0W3oUksQk0Xz8OK/giphy.gif")
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def auto(self, ctx):
-        logger.info(f"{ctx.author.name} used the auto command.")
-
         def autoMessageCheck(message: discord.Message) -> bool:
             """A message check for the auto command."""
             validResponses = ["y", "n", "yes", "no"]
@@ -129,9 +125,8 @@ class OnlyPhans(commands.Cog):
             counter += 1
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def heck(self, ctx, member: discord.Member):
-        logger.info(f"{ctx.author.name} used the heck command on {member.name}.")
-
         if member != self.bot.user:
             embed = helpers.makeSimpleTextEmbed(
                 f"Yeah! Heck {member.mention}! <a:MadNephGIF:832088703618908160>"
@@ -142,34 +137,6 @@ class OnlyPhans(commands.Cog):
             )
 
         await ctx.send(embed=embed)
-
-    # @commands.command()
-    # async def trickortreat(self, ctx):
-    #     """A surprise for users!"""
-    #     guild = ctx.guild
-    #     guildChannels = guild.channels
-    #
-    #     randomNum = random.randint(1, 1000)
-    #     if randomNum == 1000:
-    #         message = f"You've won 10g from Phan! <:NephEyes:891904818296262697> Congrats!!"
-    #         embed = self.embedMessage(message)
-    #         await ctx.send(content='<@!435141251797483540>', embed=embed)
-    #     else:
-    #         await ctx.send('<:wigglysneer:823689813474934824>')
-    #         for _ in range(10):
-    #             randomChannel = random.choice(guildChannels)
-    #             if type(randomChannel) != discord.TextChannel:
-    #                 continue
-    #             message = await randomChannel.send(f"{ctx.author.mention}")
-    #             await message.delete()
-
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        phrases = ["fractal god", "fractalgod", "fractalg0d", "fractal g0d"]
-        if message.author != self.bot.user:
-            if any(phrase in message.content.lower() for phrase in phrases):
-                logger.info(f"User {message.author.name} triggered the fractal god response.")
-                await message.channel.send("<:fractalGod:783118002009800714>")
 
 
 def setup(bot):
--- cogs/guilds/wigglyfriends.py
+++ cogs/guilds/wigglyfriends.py
@@ -1,10 +1,8 @@
 import logging
 
-import discord
-import helpers
-import requests
-from bs4 import BeautifulSoup
 from discord.ext import commands
+
+import helpers
 
 logger = logging.getLogger("main.wigglyfriends")
 
@@ -21,9 +19,8 @@ class Fun(commands.Cog):
         return ctx.guild.id == 721891483359379606
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def bap(self, ctx) -> None:
-        logger.info(f"{ctx.author.name} used the bap command.")
-
         phanID = 435141251797483540
         phanUser = await self.bot.fetch_user(phanID)
         emoji = "<a:phanbap:722239855618293760>"
@@ -31,127 +28,108 @@ class Fun(commands.Cog):
         await ctx.channel.send(f"{phanUser.mention} {emoji}")
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def corona(self, ctx):
-        logger.info(f"{ctx.author.name} used the corona command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/mG10VIX.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def traitor(self, ctx):
-        logger.info(f"{ctx.author.name} used the traitor command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/deOBda9.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def traitorti(self, ctx):
-        logger.info(f"{ctx.author.name} used the traitorti command.")
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/K9YURW6.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def shower(self, ctx):
-        logger.info(f"{ctx.author.name} used the shower command.")
-
         dot = await self.bot.fetch_user(226166683046248448)
         embed = helpers.makeSimpleTextEmbed("pls <:wuhh:724762759421624461> Go shower!")
 
         await ctx.send(f"{dot.mention}", embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def whitty(self, ctx):
-        logger.info(f"{ctx.author.name} used the whitty command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/7kVeHV3.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def kieran(self, ctx):
-        logger.info(f"{ctx.author.name} used the kieran command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/1U4AqJe.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def corn(self, ctx):
-        logger.info(f"{ctx.author.name} used the corn command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/IFitNY3.jpg")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def boxbaby(self, ctx):
-        logger.info(f"{ctx.author.name} used the boxbaby command.")
-
         embed = helpers.makeSimpleTextEmbed("A baby. In a box. Box baby.")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def stroke(self, ctx):
-        logger.info(f"{ctx.author.name} used the stroke command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/1b0yE2R.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def sexy(self, ctx):
-        logger.info(f"{ctx.author.name} used the sexy command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/r6itxP0.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def nobelprize(self, ctx):
-        logger.info(f"{ctx.author.name} used the nobelprize command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/tdZfj0Z.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def john(self, ctx):
-        logger.info(f"{ctx.author.name} used the john command.")
-
         embed = helpers.makeSimpleTextEmbed("Thank you John! :)")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def canadian(self, ctx):
-        logger.info(f"{ctx.author.name} used the canadian command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/I53BoAf.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def duality(self, ctx):
-        logger.info(f"{ctx.author.name} used the duality command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/IKgQY2g.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def chickens(self, ctx):
-        logger.info(f"{ctx.author.name} used the chickens command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/lHGL0yt.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def quarantine(self, ctx):
-        logger.info(f"{ctx.author.name} used the quarantine command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/C7RP47V.png")
         await ctx.send(embed=embed)
 
     @commands.command()
+    @helpers.logCommand(logger=logger)
     async def bape(self, ctx):
-        logger.info(f"{ctx.author.name} used the bape command.")
-
         embed = helpers.makeSimpleImageEmbed("https://i.imgur.com/dvu4guN.png")
         await ctx.send(embed=embed)
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
 
 
 def setup(bot):
